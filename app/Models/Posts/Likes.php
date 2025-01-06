8db7f2351c3a6b14b238666b53e51a2bf7e68787
Pablo Cunha <pabloncf@gmail.com>
2025-01-05 23:41:46 -0300

    Feature: Creating initial migrations

---
+++ app/Models/Posts/Likes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Posts;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Likes extends Model
+{
+    //
+}
---
+++ app/Models/Profiles/Friendships.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Profiles;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Friendships extends Model
+{
+    //
+}
