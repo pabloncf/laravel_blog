39ebf30e3355e00709aee7597246d9dcd3f591eb
Pablo Cunha <pabloncf@gmail.com>
2025-01-07 22:27:54 -0300

    Feat: Making the Login page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -8,4 +9,16 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true, // Ignora avisos de dependências
+            },
+        },
+    },
 });
